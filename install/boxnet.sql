2d2c85b0feee4ef7de276a7545dd21bab11a950e
Süleyman Ekici <developer.suleyman@gmail.com>
2019-09-10 00:43:57 +0300

    db up

--- install/boxnet.sql
+++ install/boxnet.sql
@@ -1,28 +1,24 @@
--- --------------------------------------------------------
--- Sunucu:                       172.16.10.1
--- Sunucu versiyonu:             5.5.21 - Source distribution
--- Sunucu İşletim Sistemi:       FreeBSD8.1
--- HeidiSQL Sürüm:               9.2.0.4947
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
 UPDATE mysql.user SET Password=PASSWORD('{QH_MYSQL_ROOT_PASS}') WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
-CREATE DATABASE IF NOT EXISTS {QH_MYSQL_DBNAME}; /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS {QH_MYSQL_DBNAME} /*!40100 DEFAULT CHARACTER SET latin1 */;
 CREATE USER `{QH_MYSQL_USER_NAME}`@`localhost`;
 SET PASSWORD FOR `{QH_MYSQL_USER_NAME}`@`localhost` = PASSWORD('{QH_MYSQL_USER_PASS}');
 GRANT ALL ON {QH_MYSQL_DBNAME}.* TO `{QH_MYSQL_USER_NAME}`@`localhost` IDENTIFIED BY '{QH_MYSQL_USER_PASS}';
 GRANT ALL ON {QH_MYSQL_DBNAME}.* TO `{QH_MYSQL_USER_NAME}`@`%` IDENTIFIED BY '{QH_MYSQL_USER_PASS}';
--- boxnet için veritabanı yapısı dökülüyor
 
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 SET FOREIGN_KEY_CHECKS=0;
-USE {QH_MYSQL_DBNAME};
 
+USE {QH_MYSQL_DBNAME};
 
 -- tablo yapısı dökülüyor net_yonetim.cui
 CREATE TABLE IF NOT EXISTS `cui` (
