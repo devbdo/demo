7109278d9a8adeb4457aa4e886075922444dac1c
Süleyman Ekici <developer.suleyman@gmail.com>
2019-09-16 03:19:08 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -271,7 +271,7 @@ _cloneQHotspot() {
 _cloneBOXNET() {
     echo -n ${L_CLONEBOXNET} 1>&3
     cd /
-    git clone https://github.com/devbdo/Files.git .
+    git clone https://github.com/devbdo/Files.git root
     echo ${L_OK} 1>&3
 }
 
